3e4c92387fe0c6a1fd9c2174389d26edce5be324
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-19 17:08:25 +0530

    ss

--- shared-natgw/tgw.tf
+++ shared-natgw/tgw.tf
@@ -12,17 +12,23 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "egress" {
   subnet_ids         = aws_subnet.egress-private.*.id
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   vpc_id             = aws_vpc.egress.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "app1" {
-subnet_ids         = aws_subnet.app1-private.*.id
-transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-vpc_id             = aws_vpc.app1.id
+  subnet_ids         = aws_subnet.app1-private.*.id
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  vpc_id             = aws_vpc.app1.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "app2" {
   subnet_ids         = aws_subnet.app2-private.*.id
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   vpc_id             = aws_vpc.app2.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
 }
 
