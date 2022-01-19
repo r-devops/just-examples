e560a5cd56b95926a5ea1c7dbc0dca55b9d2fa5a
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-19 16:17:32 +0530

    ss

--- shared-natgw/tgw.tf
+++ shared-natgw/tgw.tf
@@ -1,3 +1,10 @@
 resource "aws_ec2_transit_gateway" "tgw" {
   description = "TGW-Internet"
+  auto_accept_shared_attachments = "enable"
+  default_route_table_association = "enable"
+  default_route_table_propagation = "enable"
+  tags = {
+    Name = "TGW-Internet"
+  }
 }
+
