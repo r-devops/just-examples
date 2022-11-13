1a193eda0ac6b92c99776cac6e6877320535e505
Raghu K <raghuk.vit@gmail.com>
2022-11-13 23:45:46 +0530

    mm

--- vpc-with-nodejs-app/app-build/main.tf
+++ vpc-with-nodejs-app/app-build/main.tf
@@ -2,13 +2,12 @@ resource "null_resource" "build" {
   triggers = {
     always = timestamp()
   }
-  depends_on = [aws_ecr_repository.demo-app]
   provisioner "local-exec" {
     command = <<EOF
 cd ${path.root}/app-source-code
 aws ecr get-login-password | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com
-docker build -t ${aws_ecr_repository.demo-app.repository_url} .
-docker push ${aws_ecr_repository.demo-app.repository_url}
+docker build -t ${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/demo-app:latest .
+docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/demo-app:latest
 EOF
   }
 }
