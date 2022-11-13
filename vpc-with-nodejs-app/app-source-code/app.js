2a2b756cc409392ddd4d1dde4b23f19ecb04f8fe
Raghu K <raghuk.vit@gmail.com>
2022-11-13 23:54:41 +0530

    mm

--- vpc-with-nodejs-app/app-source-code/app.js
+++ vpc-with-nodejs-app/app-source-code/app.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const os = require('os');
+var ip = require("ip");
 
 console.log("bogo server starting and listening on 8080...");
 
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("Hello World, my host ip is: " + os.networkInterfaces() + "\n");
+  response.end("Hello World, my host ip is: " + ip.address() + "\n");
 };
 
 var www = http.createServer(handler);
