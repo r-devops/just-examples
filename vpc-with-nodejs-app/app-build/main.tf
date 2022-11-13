a86bb924c7bfcc087de5c1889db5a4f34c77987a
Raghu K <raghuk.vit@gmail.com>
2022-11-13 22:37:26 +0530

    mm

--- vpc-with-nodejs-app/app-build/main.tf
+++ vpc-with-nodejs-app/app-build/main.tf
@@ -15,6 +15,7 @@ resource "null_resource" "build" {
     command = <<EOF
 cd ${path.root}/app-source-code
 aws ecr get-login-password | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.region.amazonaws.com
+aws ecr get-login-password | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com
 docker build -t ${aws_ecr_repository.demo-app.repository_url} .
 docker push ${aws_ecr_repository.demo-app.repository_url}
 EOF
