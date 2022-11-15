577c61a9a1a72c77687cda50856fca5b1fecc523
Raghu K <raghuk.vit@gmail.com>
2022-11-15 19:43:43 +0530

    ss

--- vpc-with-nodejs-app/app-deploy/main.tf
+++ vpc-with-nodejs-app/app-deploy/main.tf
@@ -1,10 +1,15 @@
+### Not using ECR Terraform resource since it is not maintaining idompotency. This
 resource "null_resource" "deploy-helm-chart" {
   triggers = {
     always = timestamp()
   }
   provisioner "local-exec" {
     command = <<EOF
-helm upgrade -i demo-app ${path.module}/helm
+aws ecr create-repository --repository-name demo-app --region=us-east-1 || true
+helm upgrade -i demo-app ${path.module}/helm --set-string imageFullName=${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/demo-app
 EOF
   }
 }
+
+data "aws_caller_identity" "current" {}
+
